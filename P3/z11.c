dd369a923cc0e9f8253f89c0f29dc6f123dca5be
VladimirJovicic <vlada.jova@yahoo.com>
2014-10-26 18:28:41 +0100

    Update z11.c

--- P3/z11.c
+++ P3/z11.c
@@ -10,7 +10,10 @@ void menjaj(float *a,float *b)
 }
 void sort(float *a,int n){
     int i,j;
-    for(i=0;i<n-1;i++)for(j=i+1;j<n;j++)if(*(a+i)>*(a+j))menjaj(a+i, a+j);
+    for(i=0;i<n-1;i++)
+        for(j=i+1;j<n;j++)
+            if(*(a+i)>*(a+j))
+                menjaj(a+i, a+j);
 }
 
 int main()
