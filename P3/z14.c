d5bd8cbf5bd819fc132efe6177b6e50102edad23
LJMocic <ljubisa.mocic@gmail.com>
2014-10-26 09:26:13 +0100

    v0.4

--- P3/z14.c
+++ P3/z14.c
@@ -2,16 +2,18 @@
 #include<ctype.h>
 #define MAX 80
 int main(){
-int n,i,q=0;
-char C[MAX],A[MAX];
-printf("Unesite broj clanova niza: ");
-scanf("%d",&n);
-for(i=0;i<n;i++){
-scanf(" %c",&C[i]);
-if(isalpha(C[i])){A[q]=C[i];
-    q++;
-};
-}
-printf("Clanovi niza A su: ");
-for(i=0;i<q;i++)printf(", %c", A[i]);
+	int n,i,q=0;
+	char C[MAX],A[MAX];
+	printf("Unesite broj clanova niza: ");
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		scanf(" %c",&C[i]);
+		if(isalpha(C[i])){
+			A[q]=C[i];
+			q++;
+		};
+	}
+	printf("Clanovi niza A su: ");
+	for(i=0;i<q;i++)printf(", %c", A[i]);
+	return 0;
 }
