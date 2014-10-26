d16b3e7d33574fa84826082c76ca5b0e53cfdf47
ilijagrbic <ilijagrb.ns@gmail.com>
2014-10-26 22:01:52 +0100

    Update z8.c
    I ovaj treba proveriti, ako sitignem veceras, provericu ja :)

--- P3/z8.c
+++ P3/z8.c
@@ -1,1 +1,25 @@
-Ista prica...
+#include<stdio.h>
+
+int main()
+{
+  int n, S=0;      
+  int i,j;
+  int pom=1;
+  
+  printf("Unesite broj n:");
+  scanf("$d",&n);
+  
+  for(i=1;i<n;i++)          //u ovoj petlji sabirmo n sabiraka
+  {
+    for(j=0;j<i;j++)        //u ovoj petlji trazimo i-ti sabirak
+    {
+      pom*=(i+j+1);
+    }
+    S+=pom;
+  }
+  
+  printf("Suma je:$d",S);
+  
+  return 0;
+  
+}
