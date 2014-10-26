af99d6aa9d602dc2ce0ef74ae31be94357401848
LJMocic <ljubisa.mocic@gmail.com>
2014-10-26 09:29:41 +0100

    v0.4

--- P4/z8.c
+++ P4/z8.c
@@ -3,18 +3,17 @@
 
 int sumacif(int n)
 {
-int sum;
-if ((n>=0)&&(n<10))
-    return n;
-else {
-    sum=n%10+sumacif(n/10);
-    return sum;
-}
+	int sum;
+	if ((n>=0)&&(n<10))return n;
+	else {
+		sum=n%10+sumacif(n/10);
+		return sum;
+	}
 }
 int main(){
-int n;
-printf("Unesite n: ");
-scanf("%d",&n);
-printf("Rezultat je: %d", sumacif(n));
-return 0;
+	int n;
+	printf("Unesite n: ");
+	scanf("%d",&n);
+	printf("Rezultat je: %d", sumacif(n));
+	return 0;
 }
