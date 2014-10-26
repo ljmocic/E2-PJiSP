6a31ce3b16e74bd6a86d411968d68fb486dfd0b7
ilijagrbic <ilijagrb.ns@gmail.com>
2014-10-26 22:37:52 +0100

    Update z5.c

--- P4/z5.c
+++ P4/z5.c
@@ -1,9 +1,27 @@
 #include<stdio.h>
+#include<math.h>
 
 int prost(int n){
     int i=2;
     for(i;i<n;i++)if(n%i==0)return 0;
     return 1;
+}
+
+int prost1(int n)           //efikasniji prost-parne brojeve odma eliminisemo iz provere
+{                           //za neparne idemo samo do korena broja, jer brojevi veci od korena nece deliti n
+    if(n==2)return 1;
+    else if(n%2==0)return 0;
+    else
+    {
+        double koren=sqrt(n);
+        int i=3;                        //znamo da nije deljivo sa 2 zbog provere u 14 redu koda
+        while(i<=koren)
+        {
+            if(n%i==0) return 0;        //ako nadjemo delilac, izadjemo iz funkcije i vratimo 0, tj. broj nije prost
+        }
+        
+        return 1;           //ako je ceo while prosao, znaci da nismo nasli delilac broja (inace bi vec izasli iz funkcije)
+    }
 }
 
 int main(){
