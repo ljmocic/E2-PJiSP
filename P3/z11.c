5b9709c7558df23dd50ccbd0889562acb3b5a5f5
LJMocic <ljubisa.mocic@gmail.com>
2014-10-26 07:16:52 +0100

    0.3
    p3z11 bez funkcija
    p3z11v2 sa funkcijama

--- P3/z11.c
+++ P3/z11.c
@@ -1,27 +1,21 @@
 #include<stdio.h>
 #define MAX 30
 
-void menjaj(float *a,float *b)
-{
-    float t;
-    t=*a;
-    *a=*b;
-    *b=t;
-}
-void sort(float *a,int n){
-    int i,j;
-    for(i=0;i<n-1;i++)for(j=i+1;j<n;j++)if(*(a+i)>*(a+j))menjaj(a+i, a+j);
-}
-
 int main()
 {
-    float x[MAX];
-    int i,n;
+    float x[MAX],t;
+    int i,n,j;
     printf("Unesite broj elemenata niza za sortiranje:\n");
     scanf("%d",&n);
     printf("Unesite clanove niza: \n");
     for(i=0;i<n;i++)scanf("%f",&x[i]);
-    sort(x,n);
+    for(i=0;i<n-1;i++)
+        for(j=i+1;j<n;j++)
+            if(x[i]>x[j]){
+            t=x[i];
+            x[i]=x[j];
+            x[j]=t;
+            }
     printf("Sortirani niz je:\n");
     for(i=0;i<n;i++)printf("x[%d]=%.2f\n",i,x[i]);
     return 0;
