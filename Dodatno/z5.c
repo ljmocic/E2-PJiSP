4c3af4b942b0754854db80bd4f2247f748ab4097
LJMocic <ljubisa.mocic@gmail.com>
2014-10-28 05:35:41 +0100

    v0.2

--- Dodatno/z5.c
+++ Dodatno/z5.c
@@ -7,11 +7,12 @@ int main(){
     scanf("%d",&n);
     printf("Unesite broj kolona matrice: ");
     scanf("%d",&m);
-    printf("Unesite elemente niza:\n");
+    for(i=0;i<n;i++)b[i]=0;
+    printf("Unesite elemente matrice:\n");
     for(i=0;i<n;i++)for(j=0;j<m;j++){
         scanf("%d",&A[i][j]);
-        b[j]=S+A[i][j];
+        b[i]+=A[i][j];
         }
-    for(i=0;i<n;i++)printf("%d je zbir elemenata %d kolone\n", b[i],i+1);
+    for(i=0;i<n;i++)printf("%d je zbir elemenata %d vrste\n", b[i],i+1);
     return 0;
 }
