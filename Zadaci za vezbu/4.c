29bfe0f01e51ebc64d62e1de12884d21a6f0e8db
Ljubiša Mocić <ljubisa.mocic@gmail.com>
2014-11-02 17:21:45 +0100

    v0.1

--- Zadaci za vezbu/4.c
+++ Zadaci za vezbu/4.c
@@ -1,18 +1,10 @@
 #include<stdio.h>
 
-int main()
-{
-	int ab,c,i;
-	
-	printf("Trazeni brojevi su : \n");
-	
-	for(i=100;i<1000;i++)
-	{
-		ab=i/10;
-		c=i%10;
-		if ((ab*ab-c*c)==i)
-			printf("%d \n",i);
-	}
-	
-	return 0;
+int main(){
+    int b,i;
+    for(i=100;i<1000;i++){
+        b=pow((i/10),2)-pow(i%10,2);
+        if(i==b)printf("Ovi broj ispunjava uslov zadatka %d \n", i, b);
+    }
+    return 0;
 }
