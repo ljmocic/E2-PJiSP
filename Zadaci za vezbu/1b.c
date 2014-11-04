946f5043402c49c426e3ce1847f64542f7897fc0
Ljubiša Moćić <ljubisa.mocic@gmail.com>
2014-11-04 16:15:40 +0000

    Update 1b.c

--- Zadaci za vezbu/1b.c
+++ Zadaci za vezbu/1b.c
@@ -3,10 +3,10 @@
 int main()
 {
     int n,i;
-    float S=0.0,pom;
+    float S=0.0;
     printf("Unesi pozitivan ceo broj : ");
     scanf("%d",&n);
-    for (i=1;i<=n;i++)S+=pow(i,1.0/i);
+    for(i=1;i<=n;i++)S+=pow(i,1.0/i);
     printf("Suma je %.3f ",S);
     return 0;
 }
