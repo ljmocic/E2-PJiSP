f45e4fafc0b5c9d1fe0bb063228f753497d7edf1
Ljubiša Moćić <ljubisa.mocic@gmail.com>
2014-11-04 16:22:45 +0000

    Update 1d.c

--- Zadaci za vezbu/1d.c
+++ Zadaci za vezbu/1d.c
@@ -1,23 +1,21 @@
 #include<stdio.h>
-#include<math.h>
 
-int fakt(int n){
-    int i,R=1;
-    for(i=1;i<=n;i++)R=R*i;
-    return R;
+float fakt(int x){
+	int i;
+	float R=1.0;
+	for(i=2;i<=x;i++)R*=i;
+	return R;
 }
 
-
-int main()
-{
-    int n,i,j;
-    float S=0.0,P=0.0;
-    printf("Unesi pozitivan ceo broj : ");
-    scanf("%d",&n);
-    for (i=1;i<=n;i++){
-        for (j=1;j<=n;j++)P+=1/(float)(j+1);
-        S+=fakt(i)/P;
-    }
-    printf("Suma je %.3f ",S);
-    return 0;
+int main(){
+	int n,i,j;
+	float S=0.0,D=0.0;
+	printf("n=");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+		for(j=1;j<=i;j++)D+=1.0/(n+1);
+		S+=fakt(i)/D;
+	}
+	printf("S=%.2f\n", S);
+	return 0;
 }
