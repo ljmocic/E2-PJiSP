e91d2e5cfec7e867022d805077271985f6e043fd
Ljubiša Moćić <ljubisa.mocic@gmail.com>
2014-11-04 16:05:47 +0000

    Update 1c.c

--- Zadaci za vezbu/1c.c
+++ Zadaci za vezbu/1c.c
@@ -3,16 +3,15 @@
 
 int fakt(int n){
     int i,R=1;
-    for(i=1;i<=n;i++)R=R*i;
+    for(i=2;i<=n;i++)R*=i;
     return R;
 }
 int main()
 {
-    int n,i,c=-1;
-    float S=0.0;
+    int n,i,S=0;
     printf("Unesi pozitivan ceo broj : ");
     scanf("%d",&n);
-    for (i=1;i<=n;i++)S+=pow(c,i+1)*fakt(3*i);
-    printf("Suma je %.3f ",S);
+    for (i=1;i<=n;i++)S+=pow(-1,i+1)*fakt(3*i);
+    printf("Suma je %d ",S);
     return 0;
 }
