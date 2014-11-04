76f699e7d6ea0f37c1df941607e9d8b51de6968f
Ljubiša Moćić <ljubisa.mocic@gmail.com>
2014-11-04 15:15:48 +0000

    v0.1

---
+++ Zadaci za vezbu/13.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#define MAX 30
+
+int maxIntervala(int A[], int poc,int kraj){
+	int i,max,maxI=poc;	
+	max=A[poc];
+	for(i=poc;i<=kraj;i++){
+		if(A[i]>max){
+			maxI=i;
+			max=A[i];
+		}
+	}
+	return maxI;
+}
+int minIntervala(int A[], int poc,int kraj){
+	int i,min,minI=poc;	
+	min=A[poc];
+	for(i=poc;i<=kraj;i++){
+		if(A[i]<min){
+			minI=i;
+			min=A[i];
+		}
+	}
+	return minI;
+}
+
+int main(){
+	int A[MAX],i,n,maxI,minI,c,d,max,min;
+	printf("Unesite n: ");
+	scanf("%d",&n);
+	printf("Unesite clanove niza:\n");
+	for(i=0;i<n;i++){
+		printf("A[%d]=",i);
+		scanf("%d",&A[i]);
+	}
+	printf("Unesite c i d:");
+	scanf("%d",&c);
+	scanf("%d",&d);
+	printf("Indeks min i max elementa niza u intervalu %d,%d je %d, %d\n",c,d,minIntervala(A,c,d),maxIntervala(A,c,d));
+	return 0;
+}
