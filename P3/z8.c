66945387cf6932edecbfab3994e9bdf329515958
Ljubiša Moćić <ljubisa.mocic@gmail.com>
2015-04-02 00:54:19 +0000

    Update z8.c

--- P3/z8.c
+++ P3/z8.c
@@ -5,10 +5,8 @@ int main()
   int n, S=0;      
   int i,j;
   int pom=1;
-  
   printf("Unesite broj n:");
   scanf("%d",&n);
-  
   for(i=1;i<=n;i++)          //u ovoj petlji sabirmo n sabiraka
   {
     for(j=0;j<=i;j++)        //u ovoj petlji trazimo i-ti sabirak
@@ -18,9 +16,6 @@ int main()
     S+=pom;
     pom=1;
   }
-  
   printf("Suma je:%d",S);
-  
   return 0;
-  
 }
