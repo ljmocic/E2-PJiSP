9a575878d0a0d6aa683839ac1623d88f453c2659
Ljubiša Moćić <ljubisa.mocic@gmail.com>
2016-04-14 21:36:11 +0200

    Update z1.c

--- Dodatno/z1.c
+++ Dodatno/z1.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 int main(){
-    int i,n=6,min=0,A[]={-5,-12,-89,5,1,25};
-    for(i=0;i<n;i++)if(A[i]<min)min=A[i];
-    if(min==0)printf("Niz nema negativnih clanova.");
+    int i, n=6, min=0, A[]={-5,-12,-89,5,1,25};
+    for(i=0;i<n;i++) if(A[i]<min)min=A[i];
+    if(min==0) printf("Niz nema negativnih clanova.");
     else printf("Najveci negativni element u nizu je %d", min);
     return 0;
 }
