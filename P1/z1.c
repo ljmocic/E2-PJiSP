e0593dd86e8b5f58bd0c9ed358956235c3172e18
Ljubiša Moćić <ljubisa.mocic@gmail.com>
2016-04-16 23:23:58 +0200

    Update z1.c

--- P1/z1.c
+++ P1/z1.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <math.h>
-int main()
-{
-    float a=2.0;
-    float b=4.0;
-    float c=5.0;
-    float s,P;
+
+int main(){
+    float s, P, a=2.0, b=4.0, c=5.0;
     s=(a+b+c)/2;
     P=sqrt(s*(s-a)*(s-b)*(s-c));
     printf("P = %f\n",P);
